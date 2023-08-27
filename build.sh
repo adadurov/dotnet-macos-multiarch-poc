7b1fb187be5320226526babb82a2a6a7b4fe22df
Alexey Adadurov <alexei.adadurov@mail.ru>
2023-08-27 13:38:17 +0300

    publish each arch as single file + create universal binary

--- build.sh
+++ build.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-PUBLISH_PATH=deploy
-MULTIARCH_EXE=$PUBLISH_PATH/mac-multiarch
+SDK_VERSION=net6.0
+# =================================================================================
+# self-contained, single file
+# =================================================================================
 
+# osx-x64
+DOTNET_RID_SUFFIX=osx-x64
+
+PUBLISH_PATH=deploy-$SDK_VERSION
+PUBLISH_PATH_X64=$PUBLISH_PATH
 rm -f PUBLISH_PATH/*
-dotnet publish EntryPoint.MacOS/EntryPoint.MacOS.csproj -f net6.0 --no-self-contained \
-       -r osx-x64 -p:AssemblyNamePostfix=osx-x64 -o $PUBLISH_PATH
 
-#DYLD_PRINT_LIBRARIES=1 
+dotnet publish EntryPoint.MacOS/EntryPoint.MacOS.csproj -f $SDK_VERSION --self-contained \
+       -r $DOTNET_RID_SUFFIX -p:AssemblyNameSuffix=$DOTNET_RID_SUFFIX -o $PUBLISH_PATH
 
-dotnet publish EntryPoint.MacOS/EntryPoint.MacOS.csproj -f net6.0 --no-self-contained \
-       -r osx-arm64 -p:AssemblyNamePostfix=osx-arm64 -o $PUBLISH_PATH
+# osx-arm64
+DOTNET_RID_SUFFIX=osx-arm64
 
-#codesign -s 
+PUBLISH_PATH=deploy-$SDK_VERSION
+PUBLISH_PATH_ARM64=$PUBLISH_PATH
+rm -f PUBLISH_PATH/*
 
-rm $MULTIARCH_EXE
-lipo -create -output $MULTIARCH_EXE $PUBLISH_PATH/EntryPoint.MacOS-osx-x64 $PUBLISH_PATH/EntryPoint.MacOS-osx-arm64
+DOTNET_RID_SUFFIX=osx-arm64
+dotnet publish EntryPoint.MacOS/EntryPoint.MacOS.csproj -f $SDK_VERSION --self-contained \
+       -r $DOTNET_RID_SUFFIX -p:AssemblyNameSuffix=$DOTNET_RID_SUFFIX -o $PUBLISH_PATH
+
+UNIVERSAL_PATH=deploy-$SDK_VERSION
+MULTIARCH_EXE=$UNIVERSAL_PATH/EntryPoint.MacOS
+
+mkdir $UNIVERSAL_PATH
+lipo -create -output $MULTIARCH_EXE $PUBLISH_PATH_X64/EntryPoint.MacOS-osx-x64 $PUBLISH_PATH_ARM64/EntryPoint.MacOS-osx-arm64
+
+rm $PUBLISH_PATH_X64/EntryPoint.MacOS-osx-x64 $PUBLISH_PATH_ARM64/EntryPoint.MacOS-osx-arm64
+
 lipo -info $MULTIARCH_EXE
+file $MULTIARCH_EXE
 
 echo
 echo Running the generated binary
 echo
-$PUBLISH_PATH/mac-multiarch
+
+$MULTIARCH_EXE
